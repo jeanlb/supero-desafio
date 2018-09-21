d3ceb4cdd2be922e1b48eee9c78f850e687b8433
Jean <jean@ubuntu.ubuntu-domain>
2018-09-21 10:09:50 -0300

    Configuracao de profiles (dev, prod) implementadas; configuracao para deploy em container externo implementada

--- src/main/java/br/com/supero/SuperoDesafioApplication.java
+++ src/main/java/br/com/supero/SuperoDesafioApplication.java
@@ -5,18 +5,37 @@ package br.com.supero;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class SuperoDesafioApplication {
+public class SuperoDesafioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperoDesafioApplication.class, args);
 	}
-
+	
+	/**
+	 * Metodo sobrescrito da classe SpringBootServletInitializer,
+	 * cuja finalidade eh configurar a aplicacao para que,
+	 * em modo de producao (war gerado), possa ser utilizada
+	 * em containers externos (Wildfly, Tomcat, etc).
+	 * 
+	 * Adapted from: 
+	 * http://www.mastertheboss.com/jboss-frameworks/spring/spring-boot-hello-world-on-wildfly
+	 * 
+	 * @param SpringApplicationBuilder
+	 * @return SpringApplicationBuilder
+	 */
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SuperoDesafioApplication.class);
+    }
+ 
 	/**
 	 * Configuracao de Bean utilizado para converter entity para dto e vice-versa.
 	 * Dependencia adicionada ao Maven (org.modelmapper).
